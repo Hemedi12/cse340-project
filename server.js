d33a7a457ffb136be3c4d7b2c5c1f4b970320e45
Charles Hemedi <u084981794@byui.edu>
2025-09-25 10:18:44 +0200

    updated file

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -26,6 +27,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
+app.get("/", baseController.buildHome)
+
 app.get("/", function(req, res) {
   res.render("index", {
     title: "Home"
