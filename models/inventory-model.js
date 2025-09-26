3d90d24d28eebd2ca917401d3a7b358e5b1b0592
Charles Hemedi <u084981794@byui.edu>
2025-09-26 18:12:02 +0200

    updated

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -27,4 +27,3 @@ async function getInventoryByClassificationId(classification_id) {
 
 
 module.exports = {getClassifications, getInventoryByClassificationId};
-module.exports = {getClassifications}
